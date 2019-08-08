072e79645089c5b9e3990d613b7ffb8bd882a20a
Muhammed Younus Attari <muhammedyounusattari@gmail.com>
2019-08-08 11:54:40 +0530

    code related to scheduler for calendar and updating a status in database

--- Kastech workspace/jamaican-citizenship-ui/src/app/shared/config/host.config.ts
+++ Kastech workspace/jamaican-citizenship-ui/src/app/shared/config/host.config.ts
@@ -1,5 +1,5 @@
 export const HOST_URL = {
-   // name:"http://localhost:8080/v1/descent-form"
-    name:'http://192.168.0.108:9090/v1/descent-form'
+   name:"http://localhost:8080/v1/descent-form"
+    //name:'http://192.168.0.108:9090/v1/descent-form'
 
 };
